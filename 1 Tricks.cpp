7698fa567bcea5cbdf8689aada3fcbffeb7d6c04
vedGit77 <104839104+vedGit77@users.noreply.github.com>
2022-06-14 00:26:46 +0530

    Create 1 Tricks.cpp

---
+++ 1 Tricks.cpp
@@ -0,0 +1,3 @@
+1. { log2(x & ~(x-1)) }....this is a short cut to return the position of the 1st set bit from the end
+   eg x = 5....its binary form is 101...so ans =0
+   eg x = 8.... "   "      "    " 1000....s0 ans = 3
