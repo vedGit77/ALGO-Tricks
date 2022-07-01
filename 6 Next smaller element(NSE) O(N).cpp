71c12cc4224758b5c63955a9d65451e9878e69e8
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-07-01 15:38:22 +0530

    Update 6 Next smaller element(NSE) O(N).cpp

--- 6 Next smaller element(NSE) O(N).cpp
+++ 6 Next smaller element(NSE) O(N).cpp
@@ -11,44 +11,44 @@ void printNSE(int arr[], int n)
 
 	// iterate for rest of the elements
 	for (int i = 0; i < n; i++) 
-  {
-      int next = arr[i];
-      // if stack is empty then this element can't be NSE
-      // for any other element, so just push it to stack
-      // so that we can find NSE for it, and continue
-      if (s.empty()) 
-      {
-        s.push({ next, i });
-        continue;
-      }
+  	{
+	      int next = arr[i];
+	      // if stack is empty then this element can't be NSE
+	      // for any other element, so just push it to stack
+	      // so that we can find NSE for it, and continue
+	      if (s.empty()) 
+	      {
+		s.push({ next, i });
+		continue;
+	      }
 
-      // while stack is not empty and the top element is
-      // greater than next
-      // a) NSE for top is next, use top's index to
-      // maintain original order
-      // b) pop the top element from stack
+	      // while stack is not empty and the top element is
+	      // greater than next
+	      // a) NSE for top is next, use top's index to
+	      // maintain original order
+	      // b) pop the top element from stack
 
-      while (!s.empty() && s.top().first > next) {
-        ans[s.top().second] = next;
-        s.pop();
-      }
+	      while (!s.empty() && s.top().first > next) {
+		ans[s.top().second] = next;
+		s.pop();
+	      }
 
-      // push next to stack so that we can find NSE for it
+	      // push next to stack so that we can find NSE for it
 
-      s.push({ next, i });
+	      s.push({ next, i });
 	}
 
 	// After iterating over the loop, the remaining elements
 	// in stack do not have any NSE, so set -1 for them
 
 	while (!s.empty()) 
-  {
+        {
 		ans[s.top().second] = -1;
 		s.pop();
 	}
 
 	for (int i = 0; i < n; i++) 
-  {
+  	{
 		cout << arr[i] << " ---> " << ans[i] << endl;
 	}
 }
