89b037d94f4f2ddf7af023ca19b54b35c6d482c4
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-12-30 12:54:48 +0530

    Create 11 Binary or Modular Exponentiation.cpp

---
+++ 11 Binary or Modular Exponentiation.cpp
@@ -0,0 +1,29 @@
+Given three numbers x, y and p, compute (x^y) % p. 
+
+
+int power(long long x, unsigned int y, int p)
+{
+    int res = 1;	
+  
+    x = x % p; // Update x if it is more than or equal to p
+    
+    // y = y%p;  =>DONT DO => FATAL!
+  
+    if (x == 0) 
+        return 0; // In case x is divisible by p;
+
+    while (y > 0)
+    {
+        // If y is odd
+        if (y & 1)
+          res = (res*x) % p;    //multiply x with res =>imp
+
+        // y must be even now
+        y = y/2;
+        x = (x*x) % p;  //update x
+    }
+  
+    return res;
+}
+
+
