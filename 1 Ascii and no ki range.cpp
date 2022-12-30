2869b94149a1cef6644e50ea9d80ad69ab7022f6
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-12-30 12:28:32 +0530

    Update and rename 1 Tricks.cpp to 1 Ascii and no ki range.cpp

---
+++ 1 Ascii and no ki range.cpp
@@ -0,0 +1,6 @@
+1. ASCII....0->48
+            a->97
+            A->65
+   
+2. int range-> { -2147483648 to 2147483647 } -> ie. { 2.1 * 10^9 tak ja sakta hai}
+   long long int range -> { -(2^63) to (2^63)-1 } -> ie. {  9 * 10^18 tak ja sakta hai}
