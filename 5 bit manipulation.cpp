bda4c42fe6a49699028d692c28d68d7e7bb47517
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-12-30 12:28:56 +0530

    Update 5 bit manipulation.cpp

--- 5 bit manipulation.cpp
+++ 5 bit manipulation.cpp
@@ -1,3 +1,8 @@
+log2(x & ~(x-1)) }....this is a short cut to return the position of the 1st set bit from the end
+   eg x = 5....its binary form is 101...so ans =0
+   eg x = 8.... "   "      "    " 1000....so ans = 3
+
+
 a+b = a^b + 2*(a&b)
   
 (a+b)%m = a%m + b%m
