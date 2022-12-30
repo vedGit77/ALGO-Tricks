463c0d35bc4548eaa4e1d40c5ce84202a4746406
vedGit777 <104839104+vedGit777@users.noreply.github.com>
2022-12-30 12:31:05 +0530

    Update 5 bit manipulation.cpp

--- 5 bit manipulation.cpp
+++ 5 bit manipulation.cpp
@@ -1,4 +1,4 @@
-log2(x & ~(x-1)) }....this is a short cut to return the position of the 1st set bit from the end
+{ log2(x & ~(x-1)) }....this is a short cut to return the position of the 1st set bit from the end
    eg x = 5....its binary form is 101...so ans =0
    eg x = 8.... "   "      "    " 1000....so ans = 3
 
