a2413fbb00dc94a8f20e4ed074b4055e97cccf41
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-01-30 01:51:54 +0530

    Update 21 Boyer_Moore_Majority_Voting_Algorithm.cpp

--- 21 Boyer_Moore_Majority_Voting_Algorithm.cpp
+++ 21 Boyer_Moore_Majority_Voting_Algorithm.cpp
@@ -9,21 +9,21 @@ int findMajority(int arr[], int n)
 	int i, candidate = -1, votes = 0;
 	// Finding majority candidate
 	for (i = 0; i < n; i++) {
-      if (votes == 0) {
-        candidate = arr[i];
-        votes = 1;
-      }
-      else{
-          if (arr[i] == candidate)
-            votes++;
-          else
-            votes--;
-      }
+	      if (votes == 0) {
+		candidate = arr[i];
+		votes = 1;
+	      }
+	      else{
+		  if (arr[i] == candidate)
+		    votes++;
+		  else
+		    votes--;
+	      }
 	}
   
 	int count = 0;
 	
-  // Checking if majority candidate occurs more than n/2 times
+  	// Checking if majority candidate occurs more than n/2 times
 	for (i = 0; i < n; i++) {
 		if (arr[i] == candidate)
 			count++;
