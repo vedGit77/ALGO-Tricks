55b3d9e9477614f2ff888209f3fab8217c2076f7
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-02-06 03:09:52 +0530

    Update 19 alternative_to_kmp.cpp

--- 19 alternative_to_kmp.cpp
+++ 19 alternative_to_kmp.cpp
@@ -1,22 +1,32 @@
-string find in STL => can be used to check 
+// string find in STL => can be used to find a string in another => same use as KMP
 
+// IMP => BUT KMP IS BETTER
 
-
-
+// Time complexiy: KMP = O(N)
+// 	      : str.find() = O(N*M)....size of strings is N and M
 
 
 int main()
 {
-	string s1 = "geeksforgeeks a computer science";
+	string s1 = "geeksforgeeksfrgeeks a computer science";
 	string s2 = "geeks";
 
-	size_t found = s1.find(s2);
-	if (found != string::npos)
-		  cout << "First occurrence is " << found << endl;
+	size_t found = s1.find(s2);  		//IMP-1 : size_t
+	if (found != string::npos)   		//IMP-2 : string::npos
+		  cout << "First occurrence is " << found << endl;  	//IMP-3 : found gives the index
 
-	found = s1.find(s2, found+1);
+	found = s1.find(s2, found+1);   //IMP-4 : found+1 for next occurrence 
 	if (found != string::npos)
+		cout << "Next occurrence is " << found << endl;
+	
+	found = s1.find(s2, found+2);
+	if(found != string::npos)
 		cout << "Next occurrence is " << found << endl;
 
 	return 0;
 }
+
+
+// O/P:
+// First occurrence is 0
+// Next occurrence is 8
