9e220d1a714ced26b2a6eaf4a20bccad71cdb40d
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-10 11:24:40 +0530

    Create 27 rand().cpp

---
+++ 27 rand().cpp
@@ -0,0 +1,15 @@
+To generate random numbers:
+
+
+int a = rand(); 
+Here { 0 <= a < RAND_MAX } -> where RAND_MAX is a large value ... varies with implementation 
+
+
+int a = rand()%n;
+Here { 0 <= a < n } -> to get in range [0,n)
+
+                                        
+int range = maxi - mini + 1;
+int a = rand()%range + mini;
+Here { mini <= a <= maxi } -> to get in range [mini,maxi]
+  
