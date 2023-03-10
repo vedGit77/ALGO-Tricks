da5202c25046c151f6117f62dbcfc680507f1169
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-11 00:34:08 +0530

    Update 28 meet_in_the_middle.cpp

--- 28 meet_in_the_middle.cpp
+++ 28 meet_in_the_middle.cpp
@@ -48,8 +48,7 @@ void calcsubarray(ll a[], ll x[], int n, int c)
 // Returns the maximum possible sum less or equal to S
 ll solveSubsetSum(ll a[], int n, ll S)
 {
-	// Compute all subset sums of first and second
-	// halves
+	// Compute all subset sums of first and second halves
 	calcsubarray(a, X, n/2, 0);
 	calcsubarray(a, Y, n-n/2, n/2);
 
@@ -59,23 +58,17 @@ ll solveSubsetSum(ll a[], int n, ll S)
 	// Sort Y (we need to do doing binary search in it)
 	sort(Y, Y+size_Y);
 
-	// To keep track of the maximum sum of a subset
-	// such that the maximum sum is less than S
+	// To keep track of the maximum sum of a subset such that the maximum sum is less than S
 	ll max = 0;
 
-	// Traverse all elements of X and do Binary Search
-	// for a pair in Y with maximum sum less than S.
+	// Traverse all elements of X and do Binary Search for a pair in Y with maximum sum less than S.
 	for (int i=0; i<size_X; i++)
 	{
 		if (X[i] <= S)
 		{
-			// lower_bound() returns the first address
-			// which has value greater than or equal to
-			// S-X[i].
 			int p = lower_bound(Y, Y+size_Y, S-X[i]) - Y;
 
-			// If S-X[i] was not in array Y then decrease
-			// p by 1
+			// If S-X[i] was not in array Y then decrease p by 1
 			if (p == size_Y || Y[p] != (S-X[i]))
 				p--;
 
