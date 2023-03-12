74aed9c9aeb27ff9e959317c2a1ca7928f3c6d5a
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-12 19:46:17 +0530

    Update 30 power_set.cpp

--- 30 power_set.cpp
+++ 30 power_set.cpp
@@ -3,18 +3,44 @@ POWER SET => 2 methods:
 2. Iterative => O(N * 2^N)
   
   
-// Recursive:
+	
+	
+// Recursive/Backtrack:
 
 void findPowerSet(char* s, vector<char> &res, int n){
-		if (n == 0) {
-			for (auto i: res)
-			  cout << i;
-      cout << "\n";
-      return;
+	if (n == 0) {
+		for (auto i: res)
+			cout << i;
+		cout << "\n";
+		return;
+	}
+	
+	res.push_back(s[n - 1]);
+	findPowerSet(s, res, n - 1);
+	res.pop_back();
+	
+	findPowerSet(s, res, n - 1);
+}
+
+
+
+// Iterative:
+
+void printPowerSet(char* set, int set_size)
+{
+	// Set_size of power set of a set with set_size n is (2^n-1)
+	unsigned int pow_set_size = pow(2, set_size);
+	int counter, j;
+
+	// Run from counter 000..0 to 111..1
+	for (counter = 0; counter<pow_set_size; counter++) 
+	{
+		for (j = 0; j < set_size; j++) 
+		{
+			if (counter & (1 << j))
+				cout << set[j];
 		}
-		res.push_back(s[n - 1]);
-		findPowerSet(s, res, n - 1);
-		res.pop_back();				
-		findPowerSet(s, res, n - 1);
+		cout << endl;
+	}
 }
 
