f9f159f368c47e8c084ecbe460b93a84db0c47c7
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-12 19:15:38 +0530

    Update 29 valid_parentheses.cpp

--- 29 valid_parentheses.cpp
+++ 29 valid_parentheses.cpp
@@ -11,26 +11,37 @@ public:
     bool isValid(string s) {
         stack<char>st;
 
-        for(int i=0; i<s.size(); i++){
-            if(st.size()==0){
+        for(int i=0; i<s.size(); i++)
+        {
+            if(st.size()==0)
+            {
                 st.push(s[i]);
-            }else{
-                if(s[i]==')'){
+            }
+            else
+            {
+                if(s[i]==')')
+                {
                     if(st.top() == '(')
                         st.pop();
                     else
                         st.push(s[i]);
-                }else if(s[i]=='}'){
+                }
+                else if(s[i]=='}')
+                {
                     if(st.top() == '{')
                         st.pop();
                     else
                         st.push(s[i]);
-                }else if(s[i]==']'){
+                }
+                else if(s[i]==']')
+                {
                     if(st.top() == '[')
                         st.pop();
                     else
                         st.push(s[i]);
-                }else{
+                }
+                else
+                {
                     st.push(s[i]);
                 }
             }
