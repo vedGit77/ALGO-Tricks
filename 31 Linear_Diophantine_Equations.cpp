3346efee843e69428ca55075f3ff232bd43ac03c
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-13 18:57:11 +0530

    Update 31 Linear_Diophantine_Equations.cpp

--- 31 Linear_Diophantine_Equations.cpp
+++ 31 Linear_Diophantine_Equations.cpp
@@ -2,6 +2,7 @@
 
 
 
+
 // 1. Euclidean algorithm (GCD):
   
 //   int gcd(int a, int b)
@@ -13,12 +14,33 @@
 
 
 
+
+
+
 // 2. Extended Euclidean Algorithm: 
 
 //   finds integer coefficients x and y such that:
 //   ax + by = gcd(a, b) 
+//   here-> a,b were given ... we can find gcd(a,b) ... now x and y are unknowns -> find them
     
-//   # NOTE: for the above eqn, its guaranteed that x,y integers exist
+//   # NOTE: for the above eqn, its GUARANTEED that x,y INTEGERS exist
+
+// How to find (x,y)?
+  
+//   ax + by = gcd(a, b)  --- 1
+//   gcd(a, b) = gcd(b%a, a)   ----2 ----we know this----from euclidean algo
+  
+//   gcd(b%a, a) = (b%a)x1 + ay1   ----using 1 and 2 --- now we have (x1,y1)
+//   ax + by = (b%a)x1 + ay1   ----using 1 and 2
+  
+//   we know => dividend = divisor*quotient + remanider => use this => b = a*(b/a) + (b%a)
+
+//   ax + by = (b – (b/a)*a)x1 + ay1
+//   ax + by = a(y1 – (b/a)*x1) + bx1
+
+//   Comparing LHS and RHS,
+//   x = y1 – (b/a)*x1
+//   y = x1
     
     
     
