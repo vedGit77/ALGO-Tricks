8479c6cc18e51a0387043d09f229e904a461e5ac
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-13 19:37:57 +0530

    Update 31 Linear_Diophantine_Equations.cpp

--- 31 Linear_Diophantine_Equations.cpp
+++ 31 Linear_Diophantine_Equations.cpp
@@ -116,47 +116,36 @@ int gcd_extend(int a, int b, int& x, int& y){
 		return a;
 	}
 
-  int g = gcd_extend(b, a%b, x, y);
-  
-  int x1 = x;
-  int y1 = y;
-  
-  x = y1;
-  y = x1 - (a / b) * y1;
-  
-  return g;
+	int g = gcd_extend(b, a%b, x, y);
+
+	int x1 = x;
+	int y1 = y;
+
+	x = y1;
+	y = x1 - (a / b) * y1;
+
+	return g;
 }
 
 // Function to print the solutions of the given equations ax + by = c
 void print_solution(int a, int b, int c){
-	int x, y;
-  
 	if (a == 0 && b == 0) {
-		// Condition for infinite solutions
-		if (c == 0) {
+		if (c == 0) 
 			cout<< "Infinite Solutions Exist"<< endl;
-		}
-
-		// Condition for no solutions exist
-		else {
-			cout
-				<< "No Solution exists"
-				<< endl;
-		}
+		else 
+			cout<< "No Solution exists"<< endl;
 	}
-  
+	
+	int x, y;
 	int gcd = gcd_extend(a, b, x, y);
 
-	// Condition for no solutions exist
 	if (c % gcd != 0) {
 		cout<< "No Solution exists"<< endl;
-  }else{
-		// Print the solution
-		cout << "x = " << x * (c / gcd) << ", y = " << y * (c / gcd) << endl;
+	}else{
+		cout << "x = " << x * (c / gcd) << ", y = " << y * (c / gcd) << endl;  //x = x1 * ( c / gcd )  //y = y1 * ( c / gcd )
 	}
 }
 
-// Driver Code
 int main(void)
 {
 	int a, b, c;
