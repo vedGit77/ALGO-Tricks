1f6b2d52ea81ae8c3e0a070b4a047829204d6027
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-21 11:40:44 +0530

    Update 34 all_abt_nCr.cpp

--- 34 all_abt_nCr.cpp
+++ 34 all_abt_nCr.cpp
@@ -27,3 +27,28 @@ int fun(int n, int k)
        
 
  
+
+
+
+
+
+// Method 2: (Modular Inversion technique) => for (nCr)%m => MODULO AAYEGA since large value!
+
+// 1. General formula => nCr is ( n*(n-1)*(n-2)* … *(n-r+1) ) / (r!)
+//    sometimes => overflow out of bound. 
+//    We need to find nCr mod m so that it doesn’t overflow. 
+//    We can easily do it with modular arithmetic formula. 
+
+// 2. for => n*(n-1)*(n-2)* ... *(n-r+1) => we can use the formula =>
+//    (a*b)%m = ((a % m) * (b % m)) % m
+
+// 3. and for the 1/r! part
+//    find the modular inverse of EVERY number from 1 to r. 
+//    Then use the same formula above with a modular inverse of 1 to r. We can find modular inverse in O(r) time using  the formula, 
+
+inv[1] = 1
+inv[i] = − ⌊m/i⌋ * inv[m mod i] mod m
+To use this formula, m has to be a prime.
+In the practice problem, we need to show the answer with modulo 1000000007 which is a prime. 
+
+So, this technique will work.
