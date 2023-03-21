c8cb053dfa4b8c537de5210239b8b89ef66f9de2
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-21 12:16:04 +0530

    Update 34 all_abt_nCr.cpp

--- 34 all_abt_nCr.cpp
+++ 34 all_abt_nCr.cpp
@@ -55,3 +55,40 @@ int fun(int n, int k)
 //    To use this formula, m has to be a prime.
 //    In the practice problem, we need to show the answer with modulo 1000000007 which is a prime. 
 //    So, this technique will work.
+
+
+
+// find (nCr)%m
+int binomialCoeff(int n, int r)
+{
+	if (r > n)
+		return 0;
+	
+	long long int m = 1000000007;
+	long long int inv[r + 1] = { 0 };   //why r+1 => since this given answer for [0, k-1] => so when r+1 => answer for [0, r]
+	
+	inv[0] = 1;
+	
+	if(r+1>=2)
+		inv[1] = 1;
+
+	// modular inversion for all the numbers
+	// from 2 to r with respect to m
+	// here m = 1000000007
+	for (int i = 2; i <= r; i++) 
+		inv[i] = ( m - (m / i) * inv[m % i] % m );
+
+
+	int ans = 1;
+
+	// for 1/(r!) part
+	for (int i = 2; i <= r; i++) 
+		ans = ((ans % m) * (inv[i] % m)) % m;
+
+	// for (n)*(n-1)*(n-2)*...*(n-r+1) part
+	for (int i = n; i >= (n - r + 1); i--) 
+		ans = ((ans % m) * (i % m)) % m;
+	
+	return ans;
+}
+
