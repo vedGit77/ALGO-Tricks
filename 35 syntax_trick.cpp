67cada34d7a6fa6b8c5d0c228669409366abe710
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-04-02 00:39:02 +0530

    Update 35 syntax_trick.cpp

--- 35 syntax_trick.cpp
+++ 35 syntax_trick.cpp
@@ -1,1 +1,9 @@
+1. if we are using multiset (or multimap):
+   say we have {1,2,2,2,3,4} in the multiset
+   if we do => set_name.erase(2) => ALL 2's are erased
+   to erase a single element => set_name.erase(set_name.lower_bound(2))
+     
+2. auto itr = next(mp.begin(), k); // returns kth iterator forward from specified iterator (begin in this case) => for map/set/multimap/multiset
 
+3. auto mid = next(mp.begin(), k);
+   auto p = prev(mid, v);   // gives Vth iterator backwards from the specified iterator (mid in this case)
