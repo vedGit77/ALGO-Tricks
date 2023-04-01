63c7cd051b93281866a3771f731eaf0712505879
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-04-02 00:39:30 +0530

    Update 35 syntax_trick.cpp

--- 35 syntax_trick.cpp
+++ 35 syntax_trick.cpp
@@ -3,6 +3,8 @@
    if we do => set_name.erase(2) => ALL 2's are erased
    to erase a single element => set_name.erase(set_name.lower_bound(2))
      
+//time complexity of next/prev = O(log N)
+      
 2. auto itr = next(mp.begin(), k); // returns kth iterator forward from specified iterator (begin in this case) => for map/set/multimap/multiset
 
 3. auto mid = next(mp.begin(), k);
