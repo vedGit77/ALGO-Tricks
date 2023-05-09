7610e30d3601c868f0af24040e1c8e41697593f7
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-05-10 02:25:44 +0530

    Update 35 syntax_trick.cpp

--- 35 syntax_trick.cpp
+++ 35 syntax_trick.cpp
@@ -9,3 +9,16 @@
 
 3. auto mid = next(mp.begin(), k);
    auto p = prev(mid, v);   // gives Vth iterator backwards from the specified iterator (mid in this case)
+
+4. in set/map => we can NOT do like vectors => ie:
+   auto itr = v.begin()+4; -> {this +4} can do in vectors, but NOT in set/map
+   
+   so, if we want the last iterator -> simply do-> auto itr = s.rbegin(); ->instead of traversing whole set/map
+   
+   BUT-> another issue -> say now we want to delete this last iterator of the set
+   we can NOT do -> { auto itr = s.rebgin(); s.erase(itr); }
+   we can NOT do this because itr is NOT general iterator, its reverse iterator -> NOT used in 'erase' in cpp
+   
+   in such a case, do this-> { auto itr = prev(s.end()); s.erase(itr); }
+   this wil work!
+   
