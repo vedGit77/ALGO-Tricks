2bd4085fe549778097b9456dca5de07d1c9bbfe8
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-08-19 22:09:26 +0530

    Create 42_digit_dp_trick.cpp

---
+++ 42_digit_dp_trick.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/number-of-beautiful-integers-in-the-range/description/
+
+You are given positive integers low, high, and k.
+
+A number is beautiful if it meets both of the following conditions:
+1. The count of even digits in the number is equal to the count of odd digits.
+2. The number is divisible by k.
+
+Return the number of beautiful integers in the range [low, high].
+
+
+SOLn => see the soln on LC
+
+BUT TRICK DEKHO => 
+very confusing to solve with low and high both => too many if else
+
+TRICK => apply => fun(high) - fun(low)  => reduces the code drastically
