863e00f3d0b0e478fcc5ba76eae45f43d261dc28
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-08-29 22:35:16 +0530

    Update 44 ordered_set.cpp

--- 44 ordered_set.cpp
+++ 44 ordered_set.cpp
@@ -9,6 +9,9 @@
 // insert, erase, order_of_key, find_by_order
 
 
+//below this code, i will discuss how to implement ordered_set for duplicate elements as well
+
+
 #include <iostream>
 using namespace std;
 
@@ -39,3 +42,14 @@ int main(){
 
 	return 0;
 }
+
+
+
+//for duplicate elements, a few changes will be made:
+//1. typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set_pair;
+//   basically upar maine int likha tha-> idhar maine pair<int,int> likha hai -> and changed name from ordered_set to ordered_set_pair
+//2. order_of_key(): returns the number of elements strictly less than the key. 
+//   implementation-> pass a pair as the parameter whose (first value is number which we want the result for) and (second value will contain a negative value e.g., -1) because the inserted pairs in the ordered_set will have the value greater than equal to 0 as it denotes the indexes of the array.
+
+
+
