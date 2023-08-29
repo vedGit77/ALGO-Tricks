c0420071d9589e0ebf6671517523eb82f75a0bb6
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-08-29 22:52:25 +0530

    Update 44 ordered_set.cpp

--- 44 ordered_set.cpp
+++ 44 ordered_set.cpp
@@ -49,7 +49,40 @@ int main(){
 //1. typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set_pair;
 //   basically upar maine int likha tha-> idhar maine pair<int,int> likha hai -> and changed name from ordered_set to ordered_set_pair
 //2. order_of_key(): returns the number of elements strictly less than the key. 
-//   implementation-> pass a pair as the parameter whose (first value is number which we want the result for) and (second value will contain a negative value e.g., -1) because the inserted pairs in the ordered_set will have the value greater than equal to 0 as it denotes the indexes of the array.
+//   implementation-> 
+//   while inserting-> insert {arr[i], i} -> where i is the index
+//   while finding num of elements less than key-> pass a pair whose (first value is number which we want the result for) and (second value will contain a negative value e.g., -1) 
+//   because the inserted pairs in the ordered_set_pair will have the value greater than equal to 0 as it denotes the indexes of the array.
+//3. find_by_order(): index as a parameter and returns an iterator to the ith element (pair) in sorted order.
 
+
+
+// Header files to implement ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+// Declaring ordered_set for pair<int,int>
+typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set_pair;
+
+int main(){
+	int arr[10] = {4, 7, 2, 4, 5, 8, 4, 1, 3, 5};
+	
+	//VVIMP => DONT write=> ordered_set_pair <pair<int,int>> mySet
+	ordered_set_pair mySet;
+	
+	for(int i = 0; i < 10; i++){
+		mySet.insert({arr[i], i});
+	}
+
+	// count of elements less than 5
+	cout << "Count of elements less than 5: " <<
+			mySet.order_of_key({5, -1}) << endl;
+
+	// Print 8th element in the ordered set iterator to a pair
+	auto itr = mySet.find_by_order(8);
+	cout << "The 8th element in the ordered set is: " <<
+			(*itr).first << endl;
+	return 0;
+}
 
 
