51c7651cca535ca39e3ef29bb04eaa3b5b2034b9
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-09-23 03:02:13 +0530

    Update 13 Binary or Modular Exponentiation.cpp

--- 13 Binary or Modular Exponentiation.cpp
+++ 13 Binary or Modular Exponentiation.cpp
@@ -1,29 +1,13 @@
-Given three numbers x, y and p, compute (x^y) % p. 
+Given three numbers n, p and mod, compute (n^p) % mod. 
 
 
-int power(long long x, unsigned int y, int p)
-{
-    int res = 1;	
-  
-    x = x % p; // Update x 
-    
-    // y = y%p;  =>DONT DO => FATAL!
-  
-    if (x == 0) 
-        return 0; // In case x is divisible by p;
-
-    while (y > 0)
-    {
-        // If y is odd
-        if (y & 1)
-          res = (res*x) % p;    //multiply x with res =>imp
-
-        // y must be even now
-        y = y/2;
-        x = (x*x) % p;  //update x
-    }
-  
-    return res;
+ll funpow(ll n, ll p){
+    if(p==0)
+        return 1;
+    if(p==1)
+        return n;
+    if(p%2==0)
+        return funpow((n*n)%mod, p/2);
+    else
+        return (n*funpow((n*n)%mod, p/2))%mod;
 }
-
-
