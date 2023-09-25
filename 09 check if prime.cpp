f8cdff108f275902998749775cef52648d1d520f
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-09-25 11:56:31 +0530

    Update 09 check if prime.cpp

--- 09 check if prime.cpp
+++ 09 check if prime.cpp
@@ -1,3 +1,17 @@
+bool isPrime(int x) {
+    if(x==0 || x==1)
+        return false;
+    
+    for (int i = 2; i * i <= x; i++) {    //time complexity => sqrt(x)
+        if (x % i == 0)
+            return false;
+    }
+    
+    return true;
+}
+
+
+
 //optimized soln
 
 bool isPrime(int n)
