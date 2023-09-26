247440744879173204c1afb3e11b5821186739ea
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-09-26 12:18:32 +0530

    Update 16 Largest_Rectangle_in_Histogram.cpp

--- 16 Largest_Rectangle_in_Histogram.cpp
+++ 16 Largest_Rectangle_in_Histogram.cpp
@@ -1,59 +1,68 @@
+//VERY SIMPLE
+
 // For every bar ‘x’, we calculate the area with ‘x’ as the smallest bar in the rectangle. 
 // If we calculate the such area for every bar ‘x’ and find the maximum of all areas, our task is done. 
 
 // How to calculate the area with ‘x’ as the smallest bar? 
+// get index of 1st smaller element on left of 'x'
+// get index of 1st smaller element on right of 'x'
 
-// We need to know the index of the (first smaller than ‘x’) bar on the left of ‘x’ and the index of the (first smaller bar) on the right of ‘x’. 
-// Let us call these indexes as ‘left index’ and ‘right index’ respectively. 
-  
-// We traverse all bars from left to right and maintain a stack of bars. Every bar is pushed to stack once. 
-// A bar is popped from the stack when a bar of smaller height is seen. 
-// When a bar is popped, we calculate the area with the popped bar as the smallest bar. 
+// simple use NEXT SMALLER ELEMENT concept
 
-// How do we get the left and right indexes of the popped bar?
-// The current index tells us the right index and the index of the previous item in the stack is the left index.
+// calculate area
 
 
 
-int getMaxArea(int hist[], int n)
-{
-	stack<int> s;                // The stack holds indexes of hist[] array. The bars stored in stack are ALWAYS IN INCREASING ORDER of heights.
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        int ans = 0, n = height.size();
 
-	int max_area = 0;            // Initialize max area
-	int tp;                      // To store top of stack
-	int area_with_top;           // To store area with top bar as the smallest bar
+        stack<pair<int,int>>left_small, right_small;  //value, index
 
+        vector<int>left(n), right(n);
 
-  int i = 0;
-	while (i < n) {
-    
-		if (s.empty() || hist[s.top()] <= hist[i]){
-			s.push(i);
-	      		i++;
-	    	}
+        for(int i=0; i<n; i++){
+            if(left_small.size() == 0){
+                left_small.push({height[i], i});
+                left[i] = -1;
+            }else{
+                while(left_small.size()>0 && left_small.top().first>=height[i])
+                    left_small.pop();
+                
+                if(left_small.size()>0)
+                    left[i] = left_small.top().second;
+                else
+                    left[i] = -1;
+                
+                left_small.push({height[i], i});
+            }
+        }
 
-		// If this bar is lower than top of stack, then calculate area of rectangle with stack top as the smallest (or minimum height) bar.
-		// 'i' is 'right index' for the top and element before top in stack is 'left index'.
-		else {
-			tp = s.top(); // store the top index
-			s.pop(); // pop the top
+        for(int i=(n-1); i>=0; i--){
+            if(right_small.size() == 0){
+                right_small.push({height[i], i});
+                right[i] = n;
+            }else{
+                while(right_small.size()>0 && right_small.top().first>=height[i])
+                    right_small.pop();
+                
+                if(right_small.size()>0)
+                    right[i] = right_small.top().second;
+                else
+                    right[i] = n;
+                
+                right_small.push({height[i], i});
+            }
+        }
 
-			area_with_top = hist[tp] * (s.empty() ? i : i - s.top() - 1);  // Calculate the area with hist[tp] stack as smallest bar
+        for(int i=0;i<n;i++){
+            int till_left = left[i]+1;
+            int till_right = right[i]-1;
 
-      			max_area = max(max_area, area_with_top);
-		}
-    
-	}
+            ans = max(ans, height[i]*(till_right - till_left + 1));
+        }
 
-	// Now pop the REMAINING BARS from stack and calculate
-	// area with every popped bar as the smallest bar
-	while (s.empty() == false) {
-		tp = s.top();
-		s.pop();
-		area_with_top = hist[tp] * (s.empty() ? i : i - s.top() - 1);
-
-		max_area = max(max_area, area_with_top);
-	}
-
-	return max_area;
-}
+        return ans;
+    }
+};
