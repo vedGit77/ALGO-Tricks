b62fc768f4a28f475a5e65cc7940fd3b5136b0de
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-10-14 13:17:59 +0530

    Update 46 bit_manipulation_imp_ques.cpp

--- 46 bit_manipulation_imp_ques.cpp
+++ 46 bit_manipulation_imp_ques.cpp
@@ -1,2 +1,33 @@
-QUES 1
+// QUES 1
+// All elements appeared k times, except one wihch appears x times. Find this "one".
+
+// GENERAL TRICK:
+// works provided (x%k != 0)
+
+ int singleNumber(vector<int>& s) {
+    	vector<int> t(32);//Made a array contain 32 elements.
+    	int sz = s.size();
+    	int i, j, n;
+    	for (i = 0; i < sz; ++i)
+    	{
+    		n = s[i];
+    		for (j = 31; j >= 0; --j)
+    		{
+    			t[j] += n & 1;//Find the last digit.
+    			n >>= 1;
+    			if (!n)
+    				break;
+    	    }
+        }
+	int res = 0;
+	for (j = 31; j >= 0; --j)
+	{
+		n = t[j] % 3;//"3" represents k times. 
+		if (n)
+			res += 1 << (31 - j);
+	}
+	return res;
+}
+
+
 
