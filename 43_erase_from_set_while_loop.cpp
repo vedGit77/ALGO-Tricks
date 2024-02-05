cd675392ef8fcdfd36a54ad5f3fc0ff68b577808
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2024-02-06 02:22:05 +0530

    Update 43_erase_from_set_while_loop.cpp

--- 43_erase_from_set_while_loop.cpp
+++ 43_erase_from_set_while_loop.cpp
@@ -1,20 +1,9 @@
 //whenever I need to erase elements from a set using a while loop:
 
-WITH FOR LOOP:
-
-for (auto it = numbers.begin(); it != numbers.end(); ){   //for loop m DONT write it++
-    if (*it % 2 == 0) {
-        numbers.erase(it++);  //erase with it++
-    }else{
-        ++it;   //else mei it++
-    }
-}
-
-
-WITH WHILE LOOP:
-
 auto itr = s.begin();
-while(itr != s.end()){
-    ...some code...
-    s.erase(itr++);  //IMP trick
+while(itr!=s.end()){
+    if(..condn for erase...)
+        s.erase(itr++);
+    else
+        itr++;
 }
