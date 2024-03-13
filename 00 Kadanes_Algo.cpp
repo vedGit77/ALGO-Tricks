04bc66f5a871f73f6d9d6956e5e022c533eb2ef4
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2024-03-13 09:44:12 +0530

    Create 00 Kadanes_Algo.cpp

---
+++ 00 Kadanes_Algo.cpp
@@ -0,0 +1,16 @@
+// max subarray sum
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& v) {
+       int n = v.size();
+        int cur_max = v[0], ans = v[0];  //2 variables....curr_max and ans....REMEMBER!
+        for(int i = 1; i < n; i++)
+        {
+            cur_max = max(v[i], cur_max + v[i]);//considers both cases-> v[i] is +ive or -ive...consider 2 examples-> {4 2 -1 -3 6} and {4 2 -5 -3 -1 9}...now dry run
+            ans = max(ans, cur_max); 
+        }
+        
+        return ans;
+    }
+};
